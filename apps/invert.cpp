fec4d74b30ac468fc3282a6453f3699c626f5fee
Luis Díaz Más <piponazo@plagatux.es>
2012-08-08 19:17:44 +0200

    Added OpenCV implementation

--- apps/invert.cpp
+++ apps/invert.cpp
@@ -5,7 +5,7 @@
  *
  *  @internal
  *    Created  08/08/12
- *   Revision 08/08/12 - 16:51:45
+ *   Revision 08/08/12 - 18:39
  *   Compiler  gcc/g++
  *    Company  
  *  Copyright  Copyright (c) 2012, Luis Diaz Mas
@@ -15,40 +15,37 @@
  */
 
 #include <argtable2.h>
-#include <getopt.h>
 #include <iostream>
-
+#include <cstdlib>
 #include <emmintrin.h>
 
 #include <opencv2/core/core.hpp> // It has a typedef for uchar
 #include <opencv2/highgui/highgui.hpp>
 
-using std::cout;
-using std::cerr;
-using std::endl;
+using namespace std;
 
 struct arg_str *path = arg_str1("i", "image", "PATH",
   "Image path. It could be a gray-scale or color image");
-struct arg_int *times= arg_int0("n", "times", "INT",
-  "Number of iterations for algorithms [1]\n"); 
 struct arg_lit *show = arg_lit0("s", "show",
   "Show the original and the modified images");
 struct arg_lit *h    = arg_lit0("h", "help",
   "Show this help");
 struct arg_end *e    = arg_end(3);
 
-void *argtable[] = {path , times, show, h, e};
+void *argtable[] = {path , show, h, e};
 
-static void invertImageCPU(const uchar *src, uchar *dst, const int pixels);
-static void invertImageMMX(const uchar *src, uchar *dst, const int pixels);
-static void invertImageSSE(const uchar *src, uchar *dst, const int pixels);
+void invertImageCPU(const uchar * __restrict__ src,
+  uchar * __restrict__ dst, const int pixels);
+void invertImageMMX(const uchar * __restrict__ src,
+  uchar * __restrict__ dst, const int pixels);
+void invertImageSSE(const uchar * __restrict__ src,
+  uchar * __restrict__ dst, const int pixels);
 
 int main(int argc, char **argv)
 {
   /////////////////////////////////////////////////////////
   // Read arguments
   /////////////////////////////////////////////////////////
-  times->ival[0] = 1;
   int nerrors = arg_parse(argc, argv, argtable);
   if (h->count)
   {
@@ -65,50 +62,41 @@ int main(int argc, char **argv)
   }
 
   const int64 ticksSecond = cv::getTickFrequency();
-  cv::Mat imgSource, imgDstCPU, imgDstMMX, imgDstSSE;
-  imgSource = cv::imread(path->sval[0], CV_LOAD_IMAGE_ANYCOLOR);
-  imgDstCPU.create(imgSource.rows, imgSource.cols, imgSource.type());
-  imgDstMMX.create(imgSource.rows, imgSource.cols, imgSource.type());
-  imgDstSSE.create(imgSource.rows, imgSource.cols, imgSource.type());
+  cv::Mat iImg, imgDstCPU, imgDstCV, imgDstMMX, imgDstSSE;
+  iImg = cv::imread(path->sval[0], CV_LOAD_IMAGE_ANYCOLOR);
+  imgDstCPU.create(iImg.rows, iImg.cols, iImg.type());
+  imgDstCV.create(iImg.rows, iImg.cols, iImg.type());
+  imgDstMMX.create(iImg.rows, iImg.cols, iImg.type());
+  imgDstSSE.create(iImg.rows, iImg.cols, iImg.type());
 
-  const int nPixels=imgSource.cols*imgSource.rows*imgSource.channels();
-  int i=0;
-  double t=0, sum=0;
-  cout << "Number of iterations: " << times->ival[0] << endl;
+  const int nPixels=iImg.cols*iImg.rows*iImg.channels();
+  double t=0;
+  cout << "Image size: " << iImg.cols << "x" << iImg.rows << "x"
+       << iImg.channels()<< " = " << nPixels << endl;
 
-  for (i=0; i<times->ival[0]; i++)
-  {
-    t = (double)cv::getTickCount();
-    invertImageCPU((uchar *)imgSource.data,
-                   (uchar *)imgDstCPU.data, nPixels);
-    sum+= ((double)cv::getTickCount() - t)/ticksSecond;
-  }
-  cout << "CPU Avg/total: " << sum/times->ival[0] << "/" << sum << endl;
+  t = (double)cv::getTickCount();
+  cv::bitwise_not(iImg, imgDstCV);
+  t = ((double)cv::getTickCount() - t)/ticksSecond;
+  cout << "OCV Avg/total: " << t << endl;
 
-  sum=0;
-  for (i=0; i<times->ival[0]; i++)
-  {
-    t = (double)cv::getTickCount();
-    invertImageMMX((uchar *)imgSource.data,
-                   (uchar *)imgDstMMX.data, nPixels);
-    sum += ((double)cv::getTickCount() - t)/ticksSecond;
-  }
-  cout << "MMX Avg/total: " << sum/times->ival[0] << "/" << sum << endl;
+  t = (double)cv::getTickCount();
+  invertImageCPU(iImg.data, imgDstCPU.data, nPixels);
+  t = ((double)cv::getTickCount() - t)/ticksSecond;
+  cout << "CPU Avg/total: " << t << endl;
 
-  sum=0;
-  for (i=0; i<times->ival[0]; i++)
-  {
-    t = (double)cv::getTickCount();
-    invertImageSSE((uchar *)imgSource.data,
-                   (uchar *)imgDstSSE.data, nPixels);
-    sum += ((double)cv::getTickCount() - t)/ticksSecond;
-  }
-  cout << "SSE Avg/total: " << sum/times->ival[0] << "/" << sum << endl;
-  
+  t = (double)cv::getTickCount();
+  invertImageMMX(iImg.data, imgDstMMX.data, nPixels);
+  t = ((double)cv::getTickCount() - t)/ticksSecond;
+  cout << "MMX Avg/total: " << t << endl;
+
+  t = (double)cv::getTickCount();
+  invertImageSSE(iImg.data, imgDstSSE.data, nPixels);
+  t = ((double)cv::getTickCount() - t)/ticksSecond;
+  cout << "SSE Avg/total: " << t << endl;
 
   if (show->count==1)
   {
-    cv::imshow("source", imgSource);
+    cv::imshow("source", iImg);
     cv::imshow("CPU", imgDstCPU);
     cv::imshow("MMX", imgDstMMX);
     cv::imshow("SSE", imgDstSSE);
@@ -118,22 +106,26 @@ int main(int argc, char **argv)
   arg_freetable(argtable, sizeof(argtable)/sizeof(argtable[0]));
 }
 
-static void invertImageCPU(const uchar *src, uchar *dst, const int pixels)
+void invertImageCPU(const uchar * __restrict__ src,
+  uchar * __restrict__ dst, const int pixels)
 {
-  for (int i = 0; i < pixels; i++ )
+  register int i;
+  for (i = 0; i < pixels; i++ )
     *dst++ = 255 - *src++;
 }
 
-static void invertImageMMX(const uchar *src, uchar *dst, const int pixels)
+void invertImageMMX(const uchar * __restrict__ src,
+  uchar * __restrict__ dst, const int pixels)
 {
   // 8 pixels are processed in one loop
+  register int i;
   int nLoop = pixels/8;
   __m64* pIn = (__m64*) src;          // input pointer
   __m64* pOut = (__m64*) dst;         // output pointer
   __m64 tmp;                          // work variable
   _mm_empty();                        // Empties the multimedia state
   __m64 n1 = _mm_set_pi32(~0, ~0);
-  for ( int i = 0; i < nLoop; ++i)
+  for (i = 0; i < nLoop; ++i)
   {
     tmp = _mm_subs_pu8 (n1 , *pIn);   // Unsigned subtraction with saturation.
                                       // tmp = n1 - *pIn  for each byte.
@@ -144,16 +136,18 @@ static void invertImageMMX(const uchar *src, uchar *dst, const int pixels)
   _mm_empty();                        // Empties the multimedia state
 }
 
-static void invertImageSSE(const uchar *src, uchar *dst, const int pixels)
+void invertImageSSE(const uchar * __restrict__ src,
+  uchar * __restrict__ dst, const int pixels)
 {
   // 16 pixels are processed in one loop
+  register int i;
   int nLoop = pixels/16;
   __m128i* pIn = (__m128i*) src;      // input pointer
   __m128i* pOut = (__m128i*) dst;     // output pointer
   __m128i tmp;                        // work variable
   _mm_empty();                        // Empties the multimedia state
   __m128i n1 = _mm_set1_epi32(~0);
-  for ( int i = 0; i < nLoop; ++i)
+  for (i = 0; i < nLoop; ++i)
   {
     tmp = _mm_subs_epi8 (n1 , *pIn);  // Unsigned subtraction with saturation.
                                       // tmp = n1 - *pIn  for each byte
